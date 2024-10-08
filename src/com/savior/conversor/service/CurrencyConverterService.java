1c6194a423d115921b1432f70af6aca699af17b9
S4vi0r <gbp17@outlook.com>
2024-10-07 21:09:17 -0500

    Add input validation to currency conversion method

--- src/com/savior/conversor/service/CurrencyConverterService.java
+++ src/com/savior/conversor/service/CurrencyConverterService.java
@@ -6,12 +6,18 @@ import java.util.Scanner;
 
 public class CurrencyConverterService {
     static Scanner scanner = new Scanner(System.in);
-    public static void convert(ApiClient apiClient, String from, String to ) {
 
-        System.out.print("Enter the amount in " + from + ": ");
-        double amount = scanner.nextDouble();
-        System.out.println("Converting " + from + " to " + to + "...");
-        String rate = apiClient.getExchangeRate().conversionRates().get(to).toString();
-        System.out.println("The amount in " + to + " is: " + amount * Double.parseDouble(rate));
+    public static void convert(ApiClient apiClient, String from, String to ) {
+        
+        try {
+            System.out.print("Enter the amount in " + from + ": ");
+            double amount = scanner.nextDouble();
+            System.out.println("Converting " + from + " to " + to + "...");
+            String rate = apiClient.getExchangeRate().conversionRates().get(to).toString();
+            System.out.println("The amount in " + to + " is: " + amount * Double.parseDouble(rate));
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a valid number.");
+            scanner.next();
+        }
     }
 }
